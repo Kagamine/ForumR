5a1cbaaa60d333583e2dad430ae31719d79733be
あまみや ゆうこ <1@1234.sh>
2016-04-13 12:26:51 +0800

    Modeling

---
+++ src/ForumR/Models/ForumContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using CodeComb.AspNet.Upload;
+using CodeComb.AspNet.Upload.Models;
+
+namespace ForumR.Models
+{
+    public class ForumContext : IdentityDbContext<User, IdentityRole<long>, long>, IFileUploadDbContext
+    {
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Thread> Threads { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<File> Files { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.SetupFiles();
+
+            builder.Entity<Thread>(e =>
+            {
+                e.HasIndex(x => x.IsTop);
+                e.HasIndex(x => x.CreationTime);
+                e.HasIndex(x => x.LastReplyTime);
+                e.HasIndex(x => x.IsAnnouncement);
+            });
+
+            builder.Entity<Post>(e =>
+            {
+                e.HasIndex(x => x.Time);
+            });
+
+            builder.Entity<Forum>(e =>
+            {
+                e.Property(x => x.ParentId).IsRequired(false);
+                e.HasIndex(x => x.PRI);
+            });
+        }
+    }
+}
---
+++ src/ForumR/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+using CodeComb.AspNet.Upload.Models;
+using Newtonsoft.Json;
+
+namespace ForumR.Models
+{
+    public class User : IdentityUser<long>
+    {
+        public DateTime RegisteryTime { get; set; }
+
+        [ForeignKey("Avatar")]
+        public Guid AvatarId { get; set; }
+
+        public virtual File Avatar { get; set; }
+
+        [MaxLength(512)]
+        public string Motto { get; set; }
+    }
+}

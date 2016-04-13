4bd76d53316e9f2cd82047b10c0c3304f6223275
あまみや ゆうこ <1@1234.sh>
2016-04-13 13:44:18 +0800

    Forum controller finished

---
+++ src/ForumR/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.SignalR;
+using ForumR.Models;
+using ForumR.Hubs;
+
+namespace ForumR.Controllers
+{
+    public class BaseController : BaseController<ForumContext, User, long>
+    {
+        [FromServices]
+        public IHubContext<ForumHub> ForumHub { get; set; }
+    }
+}
---
+++ src/ForumR/Hubs/ForumHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ForumR.Hubs
+{
+    public class ForumHub : Hub
+    {
+        public void JoinGroup(string name)
+        {
+            Groups.Add(Context.ConnectionId, name);
+        }
+    }
+}
